d3793c01d4227873e3c4ead77d210bc1a9de7dc8
leam <leamelbaz@gmail.com>
2023-06-23 00:39:17 +0300

    react app
    react app

---
+++ myapp/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from './Nav';
+
+function Header() {
+    return (
+      <div className="Header">
+        
+      </div>
+    );
+  }
+  
+  export default Header;
---
+++ myapp/src/components/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Nav(props) {
+    const navStyle = {
+        background : 'light-gray',
+    }
+    return ( 
+        <nav 
+            style={navStyle}
+            className='main-nav'
+            >
+            <ul>
+                <li>{props.first}</li>
+                <li>{props.second}</li>
+                <li>{props.third}</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
